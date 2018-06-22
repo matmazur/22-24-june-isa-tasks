7a0555d64abd8ea8c09bf975db5ca4e9a2916068
Bingo Bobo <malavonek@gmail.com>
2018-06-22 20:49:57 +0200

    initial commit

---
+++ src/test/java/Task1Test.java
@@ -0,0 +1,11 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class Task1Test {
+
+    @Test
+    public void typeTrueToPass() {
+        assertTrue("write true",false);
+    }
+}
